d4da6e9e9b0e2cc15f013b50c2e9b956c8429986
zhaoyuzhong <63790773@qq.com>
2019-02-24 16:36:02 +0800

    增加素材类型DTO

---
+++ src/main/java/com/fxpt/dto/MaterialType.java
@@ -0,0 +1,25 @@
+package com.fxpt.dto;
+
+/**
+ * Created by Administrator on 2019/2/24/024.
+ */
+public class MaterialType extends BaseDTO {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
