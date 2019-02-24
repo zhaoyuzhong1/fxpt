f7f3a24cc9db57e3cc75939cda6536bad73220db
zhaoyuzhong <63790773@qq.com>
2019-02-24 16:58:34 +0800

    增加素材sql

--- src/main/java/com/fxpt/dao/MaterialDao.java
+++ src/main/java/com/fxpt/dao/MaterialDao.java
@@ -18,14 +18,14 @@ public class MaterialDao {
 
     //添加素材
     public Integer addMaterial(Material material){
-        String sql =" insert into t_material (name,imgpath,cdate,cuser,flag) values(:name,:imgpath,now(),:cuser,'0')";
+        String sql =" insert into t_material (name,imgpath,typeid,cdate,cuser,flag) values(:name,:imgpath,:typeid,now(),:cuser,'0')";
         return baseDao.insert(sql,material);
     }
 
 
     //未审核列表
     public Page<Material> getList(String search_name,Integer pagesize, Integer count) {
-        String sql = "select tm.*,DATE_FORMAT(tm.cdate,'%Y-%m-%d %H:%i:%s') as ccdate,tu.name as username from t_material tm left join t_user tu on tm.cuser=tu.id where tm.name like ? and tm.flag='0' order by tm.cdate desc ";
+        String sql = "select tm.*,DATE_FORMAT(tm.cdate,'%Y-%m-%d %H:%i:%s') as ccdate,tu.name as username,tmt.name as typename from t_material tm left join t_user tu on tm.cuser=tu.id left join t_material_type tmt on.tm.typeid=tmt.id where tm.name like ? and tm.flag='0' order by tm.cdate desc ";
         return baseDao.queryByPage(sql,Material.class,new Object[]{"%"+search_name+"%"},pagesize,count);
     }
 
