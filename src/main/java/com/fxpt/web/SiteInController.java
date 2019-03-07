4f4e5b063eed28a44171bd1150d3c6782d2c6282
zhaoyuzhong <63790773@qq.com>
2019-03-07 22:31:56 +0800

    增加签到的查询controller

---
+++ src/main/java/com/fxpt/web/SiteInController.java
@@ -0,0 +1,45 @@
+package com.fxpt.web;
+
+import com.fxpt.dao.SiteInDao;
+import com.fxpt.dto.SiteIn;
+import com.fxpt.util.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/WeChar")
+public class SiteInController {
+
+    @Autowired
+    SiteInDao siteInDao;
+
+    /**
+     * @return 页面跳转管理
+     */
+    @RequestMapping("/webSiteIn")
+    public String webSiteInIndex(){
+        return "/siteIn/index";
+    }
+
+    /**
+     * 查询签到信息列表
+     * @param search_name 搜索关键字
+     * @param pagesize 页大小
+     * @param count 每页数量
+     * @return 查询结果集
+     */
+    @ResponseBody
+    @RequestMapping("/siteInData")
+    public Map<String, Object> siteInData(String search_name, Integer pagesize, Integer count){
+        Map<String, Object> map = new HashMap<>();
+        Page<SiteIn> pageList = siteInDao.querySiteIn(pagesize,count,search_name);
+        map.put("rows", pageList.getResult());
+        map.put("total", pageList.getTotalCount());
+        return map;
+    }
+}
