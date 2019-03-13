0ab9da71f45a58f1d1d8c634da3a706bbd1ae67a
jameszyz <d>
2019-03-13 11:09:36 +0800

    增加查询和修改方法

--- src/main/java/com/fxpt/dao/RoleDao.java
+++ src/main/java/com/fxpt/dao/RoleDao.java
@@ -2,6 +2,7 @@ package com.fxpt.dao;
 
 import com.fxpt.dao.base.BaseDao;
 import com.fxpt.dto.Role;
+import com.fxpt.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,5 +19,18 @@ public class RoleDao {
     public List<Role> getList(){
         String sql = "select * from t_role order by id asc";
         return baseDao.query(sql,Role.class,new Object[]{});
+    }
+
+
+    //列表
+    public Page<Role> getList(Integer pagesize, Integer count) {
+        String sql = "select * from t_role order by id asc ";
+        return baseDao.queryByPage(sql,Role.class,new Object[]{},pagesize,count);
+    }
+
+
+    public Integer updateRole(Role role){
+        String sql ="update t_role set name=:name,price=:price,parm=:parm where id =:id ";
+        return baseDao.update(sql,role);
     }
 }
