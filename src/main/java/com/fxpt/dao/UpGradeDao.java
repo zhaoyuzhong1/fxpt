f3df2bc8abc034eeda4e7d3f3c72eec4bef89181
zhaoyuzhong <63790773@qq.com>
2019-03-17 17:43:33 +0800

    更改查询分页的SQL

--- src/main/java/com/fxpt/dao/UpGradeDao.java
+++ src/main/java/com/fxpt/dao/UpGradeDao.java
@@ -49,7 +49,7 @@ public class UpGradeDao {
         String sql = "select a.*,b.name as username,c.name as rolename,d.name as uprolename,(SELECT IFNULL(SUM(totalprice),0) FROM t_user_goods f where f.userid=a.userid and f.flag in('2','3')) as totalprice from t_upgrade a left join t_user b on a.userid=b.id left join t_role c on a.roleid=c.id left join t_role d on a.uproleid=d.id ";
         if(keyWord!=null && !keyWord.equals("")){
             sql = sql + " where a.flag='0' and b.name like ? order by a.cdate desc";
-            return baseDao.queryByPage(sql, UpGrade.class,new Object[]{"%"+keyWord+"%","%"+keyWord+"%"}, pageSize, count);
+            return baseDao.queryByPage(sql, UpGrade.class,new Object[]{"%"+keyWord+"%"}, pageSize, count);
         }else{
             sql = sql + " where a.flag='0' order by a.cdate desc";
             return baseDao.queryByPage(sql, UpGrade.class,new Object[]{}, pageSize, count);
